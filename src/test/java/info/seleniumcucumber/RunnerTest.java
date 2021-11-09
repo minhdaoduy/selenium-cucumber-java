63e399ae2e148794d9a720f69d6eea49fe0d3406
minhdao <minhdao@nvg.vn>
2021-11-09 14:05:30 +0700

    update

--- src/test/java/info/seleniumcucumber/RunnerTest.java
+++ src/test/java/info/seleniumcucumber/RunnerTest.java
@@ -1,27 +1,27 @@
-package info.seleniumcucumber;
-
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    plugin = {
-      "pretty",
-      "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-//      "json:target/reports/cucumber-json-reports/json-report.json",
-//      "html:target/reports/cucumber-html-reports/html-report.html"
-    },
-    glue = {"info.seleniumcucumber.steps"},
-//            features = {"classpath:features/"}
-//            features = {"classpath:features/my_first.feature"}
-//    features = {"src/test/resources/features/Purchase.feature"}
-    //        features = {"src/test/resources/features/api/registeruser.feature"}
-    //        features = {"src/test/resources/features/api/login.feature"}
-    //        features = {"src/test/resources/features/api/testDB.feature"}
-            features = {"src/test/resources/features/saucedemo-login.feature"}
-    //        features = {"src/test/resources/features/weather-intercept.feature"}
-    //        features = {"src/test/resources/features/ExecuteAutomationDevTools.feature"}
-    )
-public class RunnerTest {}
+//package info.seleniumcucumber;
+//
+//
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//    plugin = {
+//      "pretty",
+//      "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+////      "json:target/reports/cucumber-json-reports/json-report.json",
+////      "html:target/reports/cucumber-html-reports/html-report.html"
+//    },
+//    glue = {"info.seleniumcucumber.steps"},
+////            features = {"classpath:features/"}
+////            features = {"classpath:features/my_first.feature"}
+////    features = {"src/test/resources/features/Purchase.feature"}
+//    //        features = {"src/test/resources/features/api/registeruser.feature"}
+//    //        features = {"src/test/resources/features/api/login.feature"}
+//    //        features = {"src/test/resources/features/api/testDB.feature"}
+//            features = {"src/test/resources/features/saucedemo-login.feature"}
+//    //        features = {"src/test/resources/features/weather-intercept.feature"}
+//    //        features = {"src/test/resources/features/ExecuteAutomationDevTools.feature"}
+//    )
+//public class RunnerTest {}
